b40e82d7130dc98062273df689d7ba8df2331e71
cloudree <cloudree@gmail.com>
2016-05-05 17:58:28 +0900

    Initial

---
+++ ScoreAnalysis/frmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ScoreAnalysis
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.ebOutput = new System.Windows.Forms.TextBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(3, -1);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(196, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Image Load ...";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // ebOutput
+            // 
+            this.ebOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ebOutput.Location = new System.Drawing.Point(3, 60);
+            this.ebOutput.Multiline = true;
+            this.ebOutput.Name = "ebOutput";
+            this.ebOutput.Size = new System.Drawing.Size(196, 376);
+            this.ebOutput.TabIndex = 2;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(3, 28);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(196, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.DefaultExt = "png";
+            this.dlgOpen.FileName = "dlgOpen";
+            this.dlgOpen.Filter = "PNG files (*.png)|*.png|JPG files (*.jpg)|*.jpg|All files (*.*)|*.*";
+            this.dlgOpen.FilterIndex = 3;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panelMain.AutoScroll = true;
+            this.panelMain.Controls.Add(this.ebOutput);
+            this.panelMain.Controls.Add(this.btnProcess);
+            this.panelMain.Controls.Add(this.btnLoad);
+            this.panelMain.Location = new System.Drawing.Point(12, 12);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(203, 436);
+            this.panelMain.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 460);
+            this.Controls.Add(this.panelMain);
+            this.Name = "frmMain";
+            this.Text = "Score Analyzer for DeReSTe";
+            this.panelMain.ResumeLayout(false);
+            this.panelMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox ebOutput;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Panel panelMain;
+    }
+}
+
---
+++ ScoreAnalysis/frmMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+using AForge.Imaging.Filters;
+
+namespace ScoreAnalysis
+{
+    public partial class frmMain : Form
+    {
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult res = dlgOpen.ShowDialog();
+            if (res != DialogResult.OK)
+                return;
+
+            String filename = dlgOpen.FileName;
+            //pbImage.Load(filename);
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
